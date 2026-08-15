c4cc71902dd7ae1f6bf10739e6bbb053ac8b631c
mohit Vats <vatsmohit1234@gmail.com>
2026-08-15 12:18:50 +0530

    image validation added

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,4 +12,4 @@ def check_health():
     return {"status": "healthy"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True)
--- backend/app/routes/predict.py
+++ backend/app/routes/predict.py
@@ -1,7 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,File,UploadFile
+from PIL import Image , UnidentifiedImageError
+
+from utils.predict_utils import validateImageType
 
 predict_router = APIRouter(prefix="/predict")
 
 @predict_router.get("/")
 def get():
+    # add frontend - image ,text input , submit button,check valid photo ...
     return {"method" : "get","health":"ok"}
+
+@predict_router.post("/")
+def post(img : UploadFile = File(...)):
+
+    #read image 
+    try :
+        image = Image.open(img.file)
+    except UnidentifiedImageError:
+        return {"error" : "Unable to identify the image format."}
+    #validate image format
+    if not validateImageType(image):
+        return {"error" : "Invalid image format. Only JPEG and PNG are supported."}
+   
+    #preprocess image 
+    #use model to predict class and confidence
+    #return json with results from model
+    return {"prediction" : "AI generated image","confidence":0.9} 
+    
